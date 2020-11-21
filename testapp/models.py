d65815e1d310d1be382efc5e941f809d9f1411c8
twinklesu <ubuntu@ip-172-31-44-11.ap-northeast-2.compute.internal>
2020-11-21 10:07:44 +0000

    500 solve plz

--- testapp/models.py
+++ testapp/models.py
@@ -1,19 +1,12 @@
-# This is an auto-generated Django model module.
-# You'll have to do the following manually to clean this up:
-#   * Rearrange models' order
-#   * Make sure each model has one field with primary_key=True
-#   * Make sure each ForeignKey and OneToOneField has `on_delete` set to the desired behavior
-#   * Remove `managed = False` lines if you wish to allow Django to create, modify, and delete the table
-# Feel free to rename the models, but don't rename db_table values or field names.
 from django.db import models
 
 
 class Post(models.Model):
-    post_no = models.IntegerField(primary_key=True)
+    post_no = models.AutoField(primary_key=True)
     post_title = models.CharField(max_length=128, blank=True, null=True)
     post_content = models.CharField(max_length=1028, blank=True, null=True)
     post_id = models.CharField(max_length=200, blank=True, null=True)
-    reg_dt = models.DateTimeField(blank=True, null=True)
+    reg_dt = models.DateTimeField(auto_now_add=True)
     mod_dt = models.DateTimeField(blank=True, null=True)
     post_image = models.TextField(blank=True, null=True)
     post_image_size = models.IntegerField(blank=True, null=True)
@@ -50,3 +43,4 @@ class UserInfo(models.Model):
     class Meta:
         managed = False
         db_table = 'user_info'
+
