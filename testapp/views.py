02e41a4f16b3d69278e017d01e030c25b35bb52a
Subin Park <twinklesu914@gmail.com>
2020-11-22 01:11:14 +0900

    comment by id

--- testapp/views.py
+++ testapp/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from .models import Post, Survey, UserInfo, PostComment
 from .serializers import PostSerializer, SurveySerializer, UserInfoSerializer, RecentPostSerializer, PostCommentSerializer
 from rest_framework import viewsets, permissions
-from rest_framework.decorators import action
+from rest_framework.response import Response
 
 class PostViewSet(viewsets.ModelViewSet):
     serializer_class = PostSerializer
@@ -21,5 +21,9 @@ class RecentPostViewSet(viewsets.ModelViewSet):
     serializer_class = RecentPostSerializer
 
 class PostCommentViewSet(viewsets.ModelViewSet):
-    queryset = PostComment.objects.all()
     serializer_class = PostCommentSerializer
+    def post_comment(self, request):
+        post_id = int(request.GET['post_no'])
+        queryset = PostComment.objects.filter(post_no__exact=post_id)
+        return Response(queryset)
+
