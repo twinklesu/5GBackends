ac1b8c03ee5376cb0f3756cee123d5d8457d90bb
Subin Park <twinklesu914@gmail.com>
2020-11-24 01:59:20 +0900

    values 없이 해보자

--- testapp/views.py
+++ testapp/views.py
@@ -36,6 +36,6 @@ class GetPostCommentAPIView(APIView):
 # id 값으로 pw 리턴해서 프론트에서 매칭해서 맞는지 확인
 class LoginAPIView(APIView):
     def get(self, request, user_id):
-        serializer = UserInfoSerializer(UserInfo.objects.get(user_id__exaxt = user_id).values(user_passward), many=False)
+        serializer = UserInfoSerializer(UserInfo.objects.get(user_id__exaxt=user_id), many=False)
         return Response(serializer.data)
 
