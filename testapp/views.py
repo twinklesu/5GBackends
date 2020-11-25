c8fc56504320ad0c95507a3e6f26f1be04b03b1d
Subin Park <twinklesu914@gmail.com>
2020-11-26 00:54:18 +0900

    Update views.py

--- testapp/views.py
+++ testapp/views.py
@@ -121,7 +121,7 @@ class FashionResultAPIView(APIView):
             cursor = connection.cursor()
             strSql = "select fashion from survey_f where reg_dt < DATE_SUB(NOW(), INTERVAL 6 HOUR) group by fashion order by count(*) desc limit 4;"
             result = cursor.execute(strSql)
-            weather = cursor.fetchall()
+            fashion = cursor.fetchall()
             connection.commit()
             connection.close()
         except:
